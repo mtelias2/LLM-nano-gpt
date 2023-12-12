17d93d915dff73974b3952119186e5e1de29a0b4
mtelias2 <moutazelias@hotmail.com>
2023-12-12 19:21:22 +0400

    update llm.py

--- llm.py
+++ llm.py
@@ -34,58 +34,6 @@ block_size = 8  # the charachter block size for the transformer
 batch_size = 4  # the batch size
 
 
-def get_batch(split):
-    data = train_data if split == "train" else test_data
-    ix = torch.randint(len(data) - block_size, (batch_size,))
-    x = torch.stack([data[i : i + block_size] for i in ix])
-    y = torch.stack([data[i + 1 : i + block_size + 1] for i in ix])
-    return x, y
-
-
-class BigramModel(nn.Module):
-    def __init__(self, vocab_size):
-        super().__init__()
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
-
-    def forward(self, idx, targets=None):
-        # get scores of what comes next. I give you the columns and you give me the rows
-        logits = self.token_embedding_table(idx)
-
-        if targets is None:
-            loss = None
-        else:
-            targets = targets.view(-1)
-            B, T, C = logits.shape
-            logits = logits.view(B * T, C)
-            loss = F.cross_entropy(logits, targets)
-
-        return logits, loss
-
-    def generate(self, idx, max_new_tokens):
-        # idx is (B, T) array of indices in the current context
-        for _ in range(max_new_tokens):
-            # get the predictions
-            logits, loss = self(idx)
-            # focus only on the last time step
-            logits = logits[:, -1, :]  # becomes (B, C)
-            # apply softmax to get probabilities
-            probs = F.softmax(logits, dim=-1)  # (B, C)
-            # sample from the distribution
-            idx_next = torch.multinomial(probs, num_samples=1)  # (B, 1)
-            # append sampled index to the running sequence
-            idx = torch.cat((idx, idx_next), dim=1)  # (B, T+1)
-        return idx
-
-
-batch_size = 32
-for steps in range(10000):
-    xb, yb = get_batch("train")
-    logits, loss = model(xb, yb)
-    optimizer.zero_grad(set_to_none=True)
-    loss.backward()
-    optimizer.step()
-
-
 class BigramModel(nn.Module):
     """
     A class representing a Bigram language model.
@@ -166,7 +114,7 @@ def get_batch(split):
 @torch.no_grad()
 def estimate_loss():
     """
-    Estimate the loss of the model.
+    Estimate the loss of the model. and print every 100 steps
 
     Returns:
     - out (dict): A dictionary containing the losses for different splits.
@@ -182,3 +130,25 @@ def estimate_loss():
         out[split] = losses.mean()
     model.train()
     return out
+
+
+model = BigramModel(vocab_size)
+optimizer = adam.Adam(model.parameters(), lr=1e-3)
+
+batch_size = 32
+eval_iters = 200
+for steps in range(10000):
+    if steps % eval_iters == 0:
+        losses = estimate_loss()
+        print(steps, losses["train"], losses["val"])
+
+    xb, yb = get_batch("train")
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+# print loss
+
+context = torch.zeros((1, 1), dtype=torch.long)
+print(decoder(model.generate(context, max_new_tokens=500)[0].tolist()))
